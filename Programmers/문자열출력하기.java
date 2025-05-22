aeeae52840ed45e1714776d4b1f855671c90c3c5
JaewonYooTime <dkrkvudghk@naver.com>
2025-05-22 18:58:01 +0900

    25.05.22

---
+++ Programmers/문자열출력하기.java
@@ -0,0 +1,10 @@
+package Programmers;
+import java.util.Scanner;
+
+public class 문자열출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
