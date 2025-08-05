91235f88177f266945ffe8769481490e4be894f8
JaewonYooTime <dkrkvudghk@naver.com>
2025-08-05 11:28:13 +0900

    25.08.05

---
+++ BAEKJOON/BRONZE_2/10820.py
@@ -0,0 +1,13 @@
+import sys
+for line in sys.stdin:
+    lower = upper = digit = space = 0
+    for char in line.rstrip('\n'):
+        if char.islower():
+            lower += 1
+        elif char.isupper():
+            upper += 1
+        elif char.isdigit():
+            digit += 1
+        elif char.isspace():
+            space += 1
+    print(lower, upper, digit, space)
